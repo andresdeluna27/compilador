1d272a747e67cbbe30045ad0e6304393720c6e52
andresdeluna27 <andresdeluna27@hotmail.com>
2017-04-17 13:56:51 -0500

    identificadores

--- compilador/parse.py
+++ compilador/parse.py
@@ -31,7 +31,28 @@ def listaDeclaracion():
 def listaSentencias():
     nombre="lista-declaracion"
     agregarElemento(arbol, sentencias(), nombre)
-    return nombre      
+    return nombre
+
+def declaracion():
+    nombre=lexema[i]
+    agregarElemento(arbol, identificador(), nombre)
+
+def declaracion():
+    nombre=""
+    if tokens[i]=="int":
+          match("int")
+          nombre="int"
+          agregarElemento(arbol, listaVariables(), nombre)
+    if tokens[i]=="float":
+          match("float")
+          nombre="float"
+          agregarElemento(arbol, listaVariables(), nombre)
+    if tokens[i]=="boolean":
+          match("boolean")
+          nombre="boolean"
+          agregarElemento(arbol, listaVariables(), nombre)
+    return nombre
+          
           
 archi=open("../../lexemas.txt",'r')
 tokens=""
